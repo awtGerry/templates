a02a0df626380b0ee191242b63670fe3264210af
Victor Rodriguez <awtgerry@gmail.com>
2024-05-30 17:15:01 -0600

    iced template

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "Usefull templates"
+
+  outputs = { self, ... }:
+  {
+    templates = {
+      iced = {
+        path = ./iced;
+        description = "Template to use iced-rs";
+      };
+    };
+  };
+}
